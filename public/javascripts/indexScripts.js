d5961356fe6f362e50d45f62cfa55646dff1b5dc
Celso Rodrigues <celso.rodrigues.dev@gmail.com>
2020-12-18 16:15:28 +0000

    authen ok

--- public/javascripts/indexScripts.js
+++ public/javascripts/indexScripts.js
@@ -8,43 +8,55 @@ $(()=>{
             statusCode: {
                 200: function (response) {
                     // POST para login
-                }
+                },
+                400: alert("Serveur error")
             }
         }).done(response => {
             console.log(response)
         })
     })
 
-    $("#registerUsernameForm").change(function() {
-        $(".userNameCheck").remove();
-        $("#registerUsernameForm").removeClass("col-12");
-        $("#registerUsernameForm").addClass("col-9");
-        $("#registerUsernameForm").css("display", "inline");
-        $("#registerUsernameForm").after("<div class=\"spinner-border text-light userNameCheck\" role=\"status\">\"</div>")
-        $.ajax({
-            url: '/users/check',
-            type: 'POST',
-            data: {'info': $("#registerUsernameForm").val()},
-            statusCode: {
-                200: () => {
-                    $(".userNameCheck").remove();
-                    $("#registerUsernameForm").after("<img class='userNameCheck' height='30' margin-left='12' src='images/check.png'/>")
-                },
-                406: () => {
-                    $(".userNameCheck").remove();
-                    $("#registerUsernameForm").after("<img class='userNameCheck' height='30' margin-left='12' src='images/notcheck.png'/>")
-                }
+    var timeoutUser;
+    $('#registerUsernameForm').on('keydown',function(){
+        if(timeoutUser){ clearTimeout(timeoutUser);}
+        timeoutUser = setTimeout(function() {
+            $(".userNameCheck").remove();
+            $("#registerUsernameForm").removeClass("col-12");
+            $("#registerUsernameForm").addClass("col-9");
+            $("#registerUsernameForm").css("display", "inline");
+            if($("#registerUsernameForm").val().length < 1 ){
+                $("#registerUsernameForm").after("<img class='userNameCheck left-margin-icon' src='images/notcheck.png'/>")
+                return;
             }
-        })
+            $("#registerUsernameForm").after("<div class=\"spinner-border text-light userNameCheck\" role=\"status\">\"</div>")
+            $.ajax({
+                url: '/users/check',
+                type: 'POST',
+                data: {'info': $("#registerUsernameForm").val()},
+                statusCode: {
+                    200: () => {
+                        $(".userNameCheck").remove();
+                        $("#registerUsernameForm").after("<img class='userNameCheck left-margin-icon' src='images/check.png'/>")
+                    },
+                    406: () => {
+                        $(".userNameCheck").remove();
+                        $("#registerUsernameForm").after("<img class='userNameCheck left-margin-icon' src='images/notcheck.png'/>")
+                    }
+                }
+            })
+        },500);
     })
 
-        $("#registerEmailForm").change(function () {
+    var timeoutEmail;
+    $('#registerEmailForm').on('keydown',function(){
+        if(timeoutEmail){ clearTimeout(timeoutEmail);}
+        timeoutEmail = setTimeout(function() {
             $(".emailCheck").remove();
             $("#registerEmailForm").removeClass("col-12");
             $("#registerEmailForm").addClass("col-9");
             $("#registerEmailForm").css("display", "inline");
             if(!validateEmail($("#registerEmailForm").val())){
-                $("#registerEmailForm").after("<img class='emailCheck' height='30' margin-left='12' src='images/notcheck.png'/>")
+                $("#registerEmailForm").after("<img class='emailCheck left-margin-icon' src='images/notcheck.png'/>")
                 emailok = false;
                 return;
             }
@@ -56,25 +68,30 @@ $(()=>{
                 statusCode: {
                     200: () => {
                         $(".emailCheck").remove();
-                        $("#registerEmailForm").after("<img class='emailCheck' height='30' margin-left='12' src='images/check.png'/>")
+                        $("#registerEmailForm").after("<img class='emailCheck left-margin-icon' src='images/check.png'/>")
                     },
                     406: () => {
                         $(".emailCheck").remove();
-                        $("#registerEmailForm").after("<img class='emailCheck' height='30' margin-left='12' src='images/notcheck.png'/>")
+                        $("#registerEmailForm").after("<img class='emailCheck left-margin-icon' src='images/notcheck.png'/>")
                     }
                 }
             })
-        })
+        },500);
+    })
 
-    $("#passwordForm").change(function () {
-        $(".passwordCheck").remove();
-        $("#passwordForm").removeClass("col-12");
-        $("#passwordForm").addClass("col-9");
-        $("#passwordForm").css("display", "inline");
-        if($("#passwordForm").val().length < 4 )
-            $("#passwordForm").after("<img class='passwordCheck' height='30' margin-left='12' src='images/notcheck.png'/>")
-        else
-            $("#passwordForm").after("<img class='passwordCheck' height='30' margin-left='12' src='images/check.png'/>")
+    var timeoutPass
+    $('#passwordForm').on('keydown',function(){
+        if(timeoutPass){ clearTimeout(timeoutPass);}
+        timeoutPass = setTimeout(function() {
+            $(".passwordCheck").remove();
+            $("#passwordForm").removeClass("col-12");
+            $("#passwordForm").addClass("col-9");
+            $("#passwordForm").css("display", "inline");
+            if($("#passwordForm").val().length < 4 )
+                $("#passwordForm").after("<img class='passwordCheck left-margin-icon' src='images/notcheck.png'/>")
+            else
+                $("#passwordForm").after("<img class='passwordCheck left-margin-icon' src='images/check.png'/>")
+        },500);
     })
 
 })
