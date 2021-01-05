214d376174f007a4f183ebbf9b75fefdf5c21a38
Celso Rodrigues <celso.rodrigues.dev@gmail.com>
2021-01-05 03:50:11 +0000

    ok

---
+++ routes/assets.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const router = express.Router();
+
+router.post('/add', (req, res) => {
+
+})
+
+router.post('/remove', (req, res) => {
+
+})
+
+router.post('/comment', (req, res) => {
+
+})
+
+router.post('/rank', (req, res) => {
+
+})
+
+module.exports = router;
